0bcb980f1cea651a88a5f7bfde20faae7eddd06f
a.lutringer <arnaud.lutringer@gmail.com>
2020-01-14 00:51:21 +0100

    Logo IHM + correctif

--- public/assets/js/imageTool.js
+++ public/assets/js/imageTool.js
@@ -1,10 +1,12 @@
-function readURL(input) {
+function readURL(input, keepDisplay) {
     if (input.files && input.files[0]) {
         var reader = new FileReader();
 
         reader.onload = function (e) {
             $('#' + input.id + '-img').attr('src', e.target.result).removeClass('no-display');
-            $('#' + input.id + '_filer_title').addClass('no-display');
+            if(!keepDisplay) {
+                $('#' + input.id + '_filer_title').addClass('no-display');
+            }s
         }
 
         reader.readAsDataURL(input.files[0]);
