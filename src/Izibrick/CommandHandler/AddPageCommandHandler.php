f0436e55e0cda5aeb4f2ca97a99bac77cd35b92f
Anthony AUMETTRE <antho33@gmail.com>
2020-07-02 16:00:35 +0200

    Ajout/suppression de page

--- src/Izibrick/CommandHandler/AddPageCommandHandler.php
+++ src/Izibrick/CommandHandler/AddPageCommandHandler.php
@@ -2,9 +2,15 @@
 
 namespace App\Izibrick\CommandHandler;
 
+use App\Entity\PageTypeBlog;
+use App\Entity\PageTypeContact;
+use App\Entity\PageTypePresentation;
 use App\Entity\Site;
 use App\Entity\Page;
 use App\Izibrick\Command\AddPageCommand;
+use App\Repository\PageTypeBlogRepository;
+use App\Repository\PageTypeContactRepository;
+use App\Repository\PageTypePresentationRepository;
 use App\Repository\SiteRepository;
 use App\Repository\PageRepository;
 
@@ -20,15 +26,30 @@ class AddPageCommandHandler
     /** @var PageRepository */
     private $pageRepository;
 
+    /** @var PageTypePresentationRepository $pageTypePresentationRepository */
+    private $pageTypePresentationRepository;
+
+    /** @var PageTypeContactRepository $pageTypeContactRepository */
+    private $pageTypeContactRepository;
+
+    /** @var PageTypeBlogRepository $pageTypeBlogRepository */
+    private $pageTypeBlogRepository;
+
     /**
      * EditGlobalParametersCommandHandler constructor.
      * @param SiteRepository $siteRepository
      * @param PageRepository $pageRepository
+     * @param PageTypePresentationRepository $pageTypePresentationRepository
+     * @param PageTypeContactRepository $pageTypeContactRepository
+     * @param PageTypeBlogRepository $pageTypeBlogRepository
      */
-    public function __construct(SiteRepository $siteRepository, PageRepository $pageRepository)
+    public function __construct(SiteRepository $siteRepository, PageRepository $pageRepository, PageTypePresentationRepository $pageTypePresentationRepository, PageTypeContactRepository $pageTypeContactRepository, PageTypeBlogRepository $pageTypeBlogRepository)
     {
         $this->siteRepository = $siteRepository;
         $this->pageRepository = $pageRepository;
+        $this->pageTypePresentationRepository = $pageTypePresentationRepository;
+        $this->pageTypeContactRepository =  $pageTypeContactRepository;
+        $this->pageTypeBlogRepository = $pageTypeBlogRepository;
     }
 
     /**
@@ -44,6 +65,17 @@ class AddPageCommandHandler
         $page->setType($command->type->getId());
         $page->setNameMenu($command->name);
         $this->pageRepository->save($page);
+        if ($command->type->getId() == 2) {
+            $pageTypePresentation = new PageTypePresentation($page);
+            $this->pageTypePresentationRepository->save($pageTypePresentation);
+        } else if ($command->type->getId() == 3) {
+            $pageTypeContact = new PageTypeContact($page);
+            $this->pageTypeContactRepository->save($pageTypeContact);
+        } else if ($command->type->getId() == 4) {
+            $pageTypeBlog = new PageTypeBlog($page);
+            $this->pageTypeBlogRepository->save($pageTypeBlog);
+        }
+
     }
 
 }
--- src/Izibrick/CommandHandler/RemovePageCommandHandler.php
+++ src/Izibrick/CommandHandler/RemovePageCommandHandler.php
@@ -6,6 +6,8 @@ use App\Entity\Site;
 use App\Entity\Page;
 use App\Izibrick\Command\AddPageCommand;
 use App\Izibrick\Command\PageTypePresentationCommand;
+use App\Repository\PageTypeBlogRepository;
+use App\Repository\PageTypeContactRepository;
 use App\Repository\PageTypePresentationRepository;
 use App\Repository\SiteRepository;
 use App\Repository\PageRepository;
@@ -25,17 +27,27 @@ class RemovePageCommandHandler
     /** @var PageTypePresentationRepository */
     private $pageTypePresentationRepository;
 
+    /** @var PageTypeContactRepository $pageTypeContactRepository */
+    private $pageTypeContactRepository;
+
+    /** @var PageTypeBlogRepository $pageTypeBlogRepository */
+    private $pageTypeBlogRepository;
+
     /**
      * EditGlobalParametersCommandHandler constructor.
      * @param SiteRepository $siteRepository
      * @param PageRepository $pageRepository
      * @param PageTypePresentationRepository $pageTypePresentationRepository
+     * @param PageTypeContactRepository $pageTypeContactRepository
+     * @param PageTypeBlogRepository $pageTypeBlogRepository
      */
-    public function __construct(SiteRepository $siteRepository, PageRepository $pageRepository, PageTypePresentationRepository $pageTypePresentationRepository)
+    public function __construct(SiteRepository $siteRepository, PageRepository $pageRepository, PageTypePresentationRepository $pageTypePresentationRepository, PageTypeContactRepository $pageTypeContactRepository, PageTypeBlogRepository $pageTypeBlogRepository)
     {
         $this->siteRepository = $siteRepository;
         $this->pageRepository = $pageRepository;
         $this->pageTypePresentationRepository = $pageTypePresentationRepository;
+        $this->pageTypeContactRepository =  $pageTypeContactRepository;
+        $this->pageTypeBlogRepository = $pageTypeBlogRepository;
     }
 
     /**
@@ -56,6 +68,18 @@ class RemovePageCommandHandler
                 throw new \Exception(sprintf('Error - PageTypePresentation not found (id: %d)', $id));
             }
             $this->pageTypePresentationRepository->remove($pageTypePresentation);
+        } else if($page->getType() == 3) {
+            $pageTypeContact = $this->pageTypeContactRepository->getByPageId($id);
+            if (!$pageTypeContact) {
+                throw new \Exception(sprintf('Error - PageTypeContact not found (id: %d)', $id));
+            }
+            $this->pageTypeContactRepository->remove($pageTypeContact);
+        } else if($page->getType() == 4) {
+            $pageTypeBlog = $this->pageTypeBlogRepository->getByPageId($id);
+            if (!$pageTypeBlog) {
+                throw new \Exception(sprintf('Error - PageTypeBlog not found (id: %d)', $id));
+            }
+            $this->pageTypeBlogRepository->remove($pageTypeBlog);
         }
         $this->pageRepository->remove($page);
     }
